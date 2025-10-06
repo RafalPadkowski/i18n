9f85bef2d29c254f7472c2fdef155822ceb94eaa
Rafal Padkowski <rafaelp@poczta.onet.pl>
2025-10-06 20:57:22 +0200

    feat: add core functionality

--- src/i18n/__init__.py
+++ src/i18n/__init__.py
@@ -1,2 +1,52 @@
-def main() -> None:
-    print("Hello from i18n!")
+import gettext
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class Translation:
+    default_language = "en"
+    default_domain = "messages"
+
+    localedir: Path | None = None
+    _language: str = default_language
+    _gnutranslations: gettext.GNUTranslations | gettext.NullTranslations = (
+        gettext.NullTranslations()
+    )
+
+    @property
+    def language(self) -> str:
+        return self._language
+
+    @language.setter
+    def language(self, value: str) -> None:
+        """
+        Set the current language.
+        Create a Translations object based on the domain, localedir and language.
+        """
+        self._language = value
+
+        if self._language != self.default_language:
+            try:
+                assert self.localedir is not None
+                self._gnutranslations = gettext.translation(
+                    domain=self.default_domain,
+                    localedir=self.localedir,
+                    languages=[self._language],
+                )
+            except AssertionError:
+                print("localedir is not initialized")
+            except OSError:
+                print("no .mo file is found")
+
+    def __call__(self, message: str) -> str:
+        """
+        Return the localized translation of message based on the current language.
+        """
+        if self._language == self.default_language:
+            return message
+
+        return self._gnutranslations.gettext(message)
+
+
+tr = Translation()
